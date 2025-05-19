2deccc88b7277d9265c17e4f1e813c0e2fa6bbd3
MathyouMB <matthewmacraebovell@outlook.com>
2025-05-19 12:25:36 -0400

    Add timezone update to scheduler

--- src/infrastructure/discord/scheduler.ts
+++ src/infrastructure/discord/scheduler.ts
@@ -3,7 +3,14 @@ import { Client } from "discord.js";
 import { urgentItemsDirectMessage } from "./tasks/urgentItemsDirectMessage";
 
 export function schedule(client: Client) {
-  cron.schedule("30 8 * * *", async () => {
-    await urgentItemsDirectMessage(client);
-  });
+  // Run at 8:30 AM EST (Eastern Standard Time)
+  cron.schedule(
+    "30 8 * * *",
+    async () => {
+      await urgentItemsDirectMessage(client);
+    },
+    {
+      timezone: "America/New_York",
+    },
+  );
 }
